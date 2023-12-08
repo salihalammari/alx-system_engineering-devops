5372acca69f9902db373daec0b6d5de5556b3127
salihalammari <slammari.1337@gmail.com>
2023-12-08 09:52:26 -0800

    do

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# puppet declarative script to grab puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+#Installing flask from pip3 using puppet
+
+package { 'flask':
+ensure   => '2.1.0',
+provider => 'pip3',
 }
